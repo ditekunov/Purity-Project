acc6995c0121aea24b86c23f6fcc06d6be942e9e
d.tekunov <d.tekunov@rambler-co.ru>
2018-03-25 21:43:59 +0300

    Added tests for ListOperations

--- src/test/scala/unitTests/ListsTests.scala
+++ src/test/scala/unitTests/ListsTests.scala
@@ -1,10 +1,21 @@
 package unitTests
 
 import org.scalatest.FunSuite
+import org.scalatest.Matchers._
 import listOperations.ListProperties._
+import utils.InputException
 
 class ListsTests extends FunSuite {
-  test("Testing of get()")
 
+  final val testListOFChars = List('A', 'B', 'C', 'D')
+  final val testListOfIntegers = List(1, 2, 3, 4, 5)
 
+  test("Testing of get()") {
+
+    get(testListOFChars, 3) shouldBe 'D'
+    assertThrows[InputException] {
+      get(testListOFChars, 4)
+    }
+    get(testListOfIntegers, 3) shouldBe 4
+  }
 }
