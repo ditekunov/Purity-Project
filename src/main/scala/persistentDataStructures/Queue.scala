9e9b18553b9b023e4ac79407f66f645b343d9a84
d.tekunov <d.tekunov@rambler-co.ru>
2018-04-08 14:15:59 +0300

    Added Queue + tests

--- src/main/scala/persistentDataStructures/Queue.scala
+++ src/main/scala/persistentDataStructures/Queue.scala
@@ -1,4 +1,7 @@
 package persistentDataStructures
+import miscellaneous.DataStructureException
+import miscellaneous.DataStructuresExceptionMessages.EmptyQueueException
+
 
 class Queue[+A](in: List[A] = Nil, out: List[A] = Nil) {
 
@@ -7,6 +10,23 @@ class Queue[+A](in: List[A] = Nil, out: List[A] = Nil) {
     case (_, _) => false
   }
 
+  def enqueue[B >: A](element: B): Queue[B] = new Queue(element :: in, out)
 
+  def dequeue: (A, Queue[A]) = out match {
+    case last :: previous => (last, new Queue(in, previous))
+    case Nil => in.reverse match {
+      case last :: previous => (last, new Queue(in, previous))
+      case Nil => throw new DataStructureException(EmptyQueueException)
+    }
+  }
+
+  def first: A = dequeue match {case (element, _) => element}
+
+  def init: Queue[A] = dequeue match {case (_, element) => element }
 
 }
+
+
+  object Queue { def createEmptyQueue[A]: Queue[A] = new Queue() }
+
+
