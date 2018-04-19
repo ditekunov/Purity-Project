02adf0e556e7a425f5c8e1429f28adeee8b2747c
Tekunov Daniil <ditekunov@edu.hse.ru>
2018-04-19 10:48:14 +0300

    Added rational arithmetics

---
+++ src/main/scala/rationalOperations/Rational.scala
@@ -0,0 +1,39 @@
+package rationalOperations
+
+import integerOperations.IntegerProperties._
+
+class Rational(numerator: Int, denominator: Int) {
+  require(denominator != 0)
+
+  private val gcd = numerator.abs gcdWith denominator.abs
+
+  private val num: Int = numerator / gcd
+
+  private val denom: Int = denominator / gcd
+
+  override def toString: String = num + "/" + denom
+
+  def this(firstInt: Int) = this(firstInt, 1)
+
+  def +(secondRational: Rational) = new Rational(
+    num * secondRational.denom + secondRational.num * denom,
+    denom * secondRational.denom)
+
+  def *(secondRational: Rational) = new Rational(
+    num * secondRational.num,
+    denom * secondRational.denom)
+
+  def /(secondRational: Rational) = new Rational(
+    num * secondRational.denom,
+    denom * secondRational.num)
+
+  def -(secondRational: Rational) = new Rational(
+    num * secondRational.denom - secondRational.num * denom,
+    denom * secondRational.denom)
+
+  def >(secondRational: Rational): Boolean =
+    this.num * secondRational.denom > this.denom * secondRational.num
+
+  def <(secondRational: Rational): Boolean =
+    this.num * secondRational.denom < this.denom * secondRational.num
+}
