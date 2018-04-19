c27355d1795f541b7199b91171b56a91dbc43c49
Tekunov Daniil <ditekunov@edu.hse.ru>
2018-04-19 12:00:53 +0300

    Added Math constants.

---
+++ src/main/scala/utils/MathConstants.scala
@@ -0,0 +1,49 @@
+package utils
+
+/**
+  * Contains some important math constants.
+  *
+  * Purity project by Daniil Tekunov.
+  */
+object MathConstants {
+
+  /**
+    * https://en.wikipedia.org/wiki/Pi
+    */
+  final val pi:              Double = 3.141592653589793
+
+  /**
+    * https://en.wikipedia.org/wiki/Turn_(geometry)#Tau_proposals
+    */
+  final val tau:             Double = 6.283185307179586
+
+  /**
+    * https://en.wikipedia.org/wiki/E_(mathematical_constant)
+    */
+  final val e:               Double = 2.718281828459045
+
+  /**
+    * https://en.wikipedia.org/wiki/Square_root_of_2
+    */
+  final val PythagorasConst: Double = 1.414213562373095
+
+  /**
+    * https://en.wikipedia.org/wiki/Square_root_of_3
+    */
+  final val TheodorusConst:  Double = 1.732050807568877
+
+  /**
+    * https://en.wikipedia.org/wiki/Euler–Mascheroni_constant
+    */
+  final val gamma:           Double = 0.577215664901532
+
+  /**
+    * https://en.wikipedia.org/wiki/Golden_ratio
+    */
+  final val phi:      Double = 1.618033988749894
+
+  /**
+    * https://en.wikipedia.org/wiki/Plastic_number
+    */
+  final val plasticNumber:   Double = 1.324717957244746
+}
