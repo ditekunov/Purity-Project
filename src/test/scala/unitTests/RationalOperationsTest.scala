267589528ad15e0bbed46d12cf4729e5c0014703
Tekunov Daniil <ditekunov@edu.hse.ru>
2018-05-16 23:46:58 +0300

    Added basic tests for rational arithmetic

---
+++ src/test/scala/unitTests/RationalOperationsTest.scala
@@ -0,0 +1,18 @@
+package unitTests
+
+import org.scalatest.Matchers._
+import org.scalatest.FunSuite
+import rationalOperations.Rational
+
+class RationalOperationsTests extends FunSuite {
+
+  val firstRational = new Rational(12,1)
+  val secondRational = new Rational(8,2)
+
+  test("Testing of the basic rational operations") {
+    (firstRational / secondRational).toString shouldBe "3/1"
+    (firstRational * secondRational).toString shouldBe "48/1"
+    (firstRational - secondRational).toString shouldBe "3/1"
+    (firstRational + secondRational).toString shouldBe "3/1"
+  }
+}
