152111b7732e4a380e7907676703ac20ac124528
Tekunov Daniil <ditekunov@edu.hse.ru>
2018-05-19 22:33:51 +0300

    Added inc, toDegrees and abs in Double

--- src/main/scala/doubleOperations/DoubleProperties.scala
+++ src/main/scala/doubleOperations/DoubleProperties.scala
@@ -2,6 +2,7 @@ package doubleOperations
 
 import utils.InputException
 import utils.ExceptionMessages.ZeroInput
+import utils.MathConstants.pi
 
 /**
   * Contains functions, affecting double properties.
@@ -24,6 +25,23 @@ class DoubleProperties(val firstDouble: Double) {
     * Returns square of a Double
     */
   def sqrDouble: Double = firstDouble * firstDouble
+ 
+  /**
+    * Returns absolute value of a Double
+    */
+  def abs: Double = if (firstDouble < 0) -firstDouble else firstDouble
+ 
+  /**
+    * Returns double, converted to degrees
+    */
+  def toDegrees: Double = firstDouble * 180.0 / pi
+ 
+  /**
+    * Increments double
+    */
+  def incDouble: Double =
+    if (a != Double.MAX_VALUE) a + 1
+    else throw new InputException(OverflowInput)
 
 }
 
