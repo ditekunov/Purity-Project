ad32ccdd3565b2d03682434ee70ab57a3ddaf91d
Tekunov Daniil <ditekunov@yandex.ru>
2018-05-26 16:22:34 +0300

    Added basic functions

--- src/main/scala/dataStructures/RedBlackTree.scala
+++ src/main/scala/dataStructures/RedBlackTree.scala
@@ -10,5 +10,30 @@ case object Red extends Color
 case object Black extends Color
 
 abstract sealed class RedBlackTree[+Int] {
+ 
+ /**
+    * Color of a tree
+    */
+  def color: Color
+
+  /**
+    * Current value
+    */
+  def value: Int
+
+  /**
+    * The left branch of a tree
+    */
+  def left: RedBlackTree[Int]
+
+  /**
+    * The right branch of a tree
+    */
+  def right: RedBlackTree[Int]
+
+  /**
+    * Checks whether this tree is empty or not
+    */
+  def isEmpty: Boolean
 
 }
