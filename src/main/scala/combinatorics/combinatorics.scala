877b5a539db7479834cf7eb52538e9ce7acf779d
Tekunov Daniil <ditekunov@yandex.ru>
2018-05-27 14:26:42 +0300

    added functions with repeats

--- src/main/scala/combinatorics/combinatorics.scala
+++ src/main/scala/combinatorics/combinatorics.scala
@@ -1,13 +1,12 @@
- 
 package object combinatorics {
-  
+
   /**
     * Contains basic combinatorics formulas.
     *
     * Purity project by Daniil Tekunov.
     */
   import integerOperations.IntegerProperties._
-   
+
   /**
     * Returns number of permutations of n objects.
     */
@@ -16,10 +15,21 @@ package object combinatorics {
   /**
     * Returns number of accomodations of n objects at m places.
     */
-  def accomodations(m: Int, n: Int) = (n!) / ((n - m)!)
+  def accomodations(m: Int, n: Int): Int = (n!) / ((n - m)!)
 
   /**
     * Returns number of combinations of n objects at m places.
     */
   def combinations(m : Int, n: Int) = (n!) / ( ((n - m)!) * (m!) )
+
+  /**
+    * Returns number of combinations with repeats of n objects at m places.
+    */
+  def combinationsWithRepeats(m : Int, n: Int): Int = ((n + m - 1)!) / ((m!) * (n - 1)!)
+
+  /**
+    * Returns number of accomodations with repeats of n objects at m places.
+    */
+  def accomodationsWithRepeats(m : Int, n: Int): Int = n.powN(m)
 }
+
