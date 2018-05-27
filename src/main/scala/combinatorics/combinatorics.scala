19454b573148e78963dd7146c5c9cec79817e6d1
Tekunov Daniil <ditekunov@yandex.ru>
2018-05-27 14:14:20 +0300

    added docs

--- src/main/scala/combinatorics/combinatorics.scala
+++ src/main/scala/combinatorics/combinatorics.scala
@@ -1,18 +1,25 @@
+ 
 package object combinatorics {
-
-import integerOperations.IntegerProperties._
   
   /**
     * Contains basic combinatorics formulas.
     *
     * Purity project by Daniil Tekunov.
     */
+  import integerOperations.IntegerProperties._
+   
+  /**
+    * Returns number of permutations of n objects.
+    */
   def permutationsCount(n: Int): Int = n!
 
+  /**
+    * Returns number of accomodations of n objects at m places.
+    */
   def accomodations(m: Int, n: Int) = (n!) / ((n - m)!)
 
+  /**
+    * Returns number of combinations of n objects at m places.
+    */
   def combinations(m : Int, n: Int) = (n!) / ( ((n - m)!) * (m!) )
-
-  
-
 }
