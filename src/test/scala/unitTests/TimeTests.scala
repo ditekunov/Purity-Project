877e9effbd6dd4f049c83e5c8edc2b1abba30dec
Tekunov Daniil <ditekunov@yandex.ru>
2018-06-04 19:17:32 +0300

    Update TimeTests.scala

--- src/test/scala/unitTests/TimeTests.scala
+++ src/test/scala/unitTests/TimeTests.scala
@@ -3,11 +3,18 @@ package unitTests
 import org.scalatest.Matchers._
 import org.scalatest.FunSuite
 import utils.InputException
-
+import time.Time._
 
 
 class TimeTests extends FunSuite {
 
-  
-  
+  test("Testing of basic time functions") {
+    10.hoursToSeconds shouldBe 3600
+    360.secondsToHours shouldBe 1
+    60.secondsToMinutes shouldBe 1
+    10.minutesToSeconds shouldBe 60
+
+    assertThrows[InputException] {
+      -1.secondsToMinutes
+    }
 }
