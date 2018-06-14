574c6e7ba8b5c65eb396dea494b243344231d29e
Tekunov Daniil <ditekunov@yandex.ru>
2018-06-14 10:27:06 +0300

    Refactor + code style

--- src/main/scala/asynchronous/UsefulFuture.scala
+++ src/main/scala/asynchronous/UsefulFuture.scala
@@ -8,7 +8,7 @@ import scala.util.{Failure, Success}
 package object futureHelper {
 
   implicit class NiceFuture[T](future: Future[T])(implicit ec: ExecutionContext) {
-    
+
     def bypassOnComplete[A](function: T => A): Future[T] = {
       future onComplete {
         case Success(outcome) => function(outcome)
@@ -16,8 +16,8 @@ package object futureHelper {
       }
       future
     }
-    
-     def ifFailure(functionFail: => Unit, exception: Option[Throwable] = None): Future[T] = {
+
+    def ifFailure(functionFail: => Unit, exception: Option[Throwable] = None): Future[T] = {
       future transformWith {
         case Failure(ex) =>
           functionFail
@@ -26,18 +26,19 @@ package object futureHelper {
         case _ => future
       }
     }
-    
-    def result(awaitTime: Long, timeUnit: TimeUnit = TimeUnit.MICROSECONDS): T = 
+
+    def result(awaitTime: Long, timeUnit: TimeUnit = TimeUnit.MICROSECONDS): T =
       Await.result(future, Duration(awaitTime, timeUnit))
-    
+
+
     def completeAndThen(anotherFuture: Future[T]): Future[T] = {
       future andThen {
         case Success(_) => anotherFuture
         case Failure(ex) => ex
       }
     }
-    
-    def completeThenCompleteThis(anotherFuture: Future[T]): Future[T] = {
+
+    def completeAndThenComplete(anotherFuture: Future[T]): Future[T] = {
       future andThen {
         case Success(_) => anotherFuture andThen {
           case Success(x) => x
@@ -45,5 +46,6 @@ package object futureHelper {
         }
         case Failure(ex) => ex
       }
+    }
   }
 }
