b7fb665b0b2c39b83a42a8ca474e2a966a050a24
Tekunov Daniil <ditekunov@yandex.ru>
2018-06-14 11:20:08 +0300

    Added doc

--- src/main/scala/asynchronous/UsefulOption.scala
+++ src/main/scala/asynchronous/UsefulOption.scala
@@ -1,21 +1,33 @@
 object UsefulOption {
 
+  /**
+    * Contains useful functions for built-in Int type
+    */
   implicit class IntOption(input: Option[Int]) {
     def getOrZero: Int = input.getOrElse(0)
 
     def getOrMax: Int = input.getOrElse(2147483647)
   }
 
+  /**
+    * Contains useful functions for built-in Long type
+    */
   implicit class LongOption(input: Option[Long]) {
     def getOrZeroL: Long = input.getOrElse(0L)
 
     def getOrMaxL: Long = input.getOrElse(9223372036854775807L)
   }
 
+  /**
+    * Contains useful functions for built-in String type
+    */
   implicit class StringOption(input: Option[String]) {
     def getOrEmpty: String = input.getOrElse("")
   }
 
+  /**
+    * Contains useful functions for throwable type
+    */
   implicit class ThrowableOption[T](input: Option[T]) {
     def getOrThrow(ex: Throwable) = input.getOrElse(ex)
   }
