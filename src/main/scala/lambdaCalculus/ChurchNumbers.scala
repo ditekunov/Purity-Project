860fcc71207ce9ab80f352ed7ec1334a33da9e8b
Tekunov Daniil <ditekunov@yandex.ru>
2018-06-21 11:21:17 +0300

    Added 'multiplication' operation

--- src/main/scala/lambdaCalculus/ChurchNumbers.scala
+++ src/main/scala/lambdaCalculus/ChurchNumbers.scala
@@ -21,6 +21,11 @@ object ChurchNumbers {
     * Realizes "addition" operation, that adds a number to another number in Church numbers
     */
   def addition[T](n: λ_λ_T[T])(m: λ_λ_T[T]): λ_λ_T[T] = (s: λ_T[T]) => (z: T) => m(s)( n(s)(z) )
+  
+  /**
+    * Realizes "multiplication" operation, that multiplies a number with another number in Church numbers
+    */
+  def multiplication[T](n: λ_λ_T[T])(m: λ_λ_T[T]): λ_λ_T[T] = (s: λ_T[T]) => (z: T) => n(m(s))(z)
 
 
   def one_succ[T]: λ_λ_T[T] = successor(zero)
