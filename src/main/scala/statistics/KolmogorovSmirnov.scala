c78597cd04eaf6e6fc1764f40be79db5d581dfac
Tekunov Daniil <ditekunov@yandex.ru>
2018-06-25 14:39:13 +0300

    Added Kolmogorov-Smirnov test

---
+++ src/main/scala/statistics/KolmogorovSmirnov.scala
@@ -0,0 +1,27 @@
+import scala.math.{abs, sqrt}
+
+object KolmogorovSmirnovTest {
+
+  /**
+    * Returns, whether the distribution of the value increased or not, using Kolmogorov-Smirnov test.
+    */
+  def ColmogorovSmirnov(n: List[Int], m: List[Int]): Boolean = {
+
+    val nLen = n.length
+
+    val mLen = m.length
+
+    val tRange = n ++ m
+
+    val t = tRange.min until tRange.max
+
+    (for (i <- t) yield {
+      val localN = n.count { x => x <= i } / nLen
+
+      val localM = m.count { x => x <= i } / mLen
+
+      (localN - localM).abs
+
+    }).max > 1.36 * sqrt((nLen + mLen) / (nLen * mLen))
+  }
+}
